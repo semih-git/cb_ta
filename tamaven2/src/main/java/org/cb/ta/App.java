6de7a230cb3dde0f3d5ffd03bd79f33b4a0c5040
semih-git <cemcan34355@gmail.com>
2020-11-22 21:23:16 +0100

    i added commit.

--- tamaven2/src/main/java/org/cb/ta/App.java
+++ tamaven2/src/main/java/org/cb/ta/App.java
@@ -1,6 +1,10 @@
 package org.cb.ta;
 
+import java.util.ArrayList;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
@@ -9,10 +13,52 @@ import org.openqa.selenium.chrome.ChromeDriver;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws InterruptedException
     {
-    	System.setProperty("webdriver.chrome.driver", "C:\\TA_sesions\\FirstTestAutomation2\\tamaven2\\webdrivers\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();		
-		driver.get("https://www.google.com");
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\laptop\\git\\cb_ta\\tamaven2\\webdrivers\\chromedriver.exe");
+    	
+    	WebDriver driver = new ChromeDriver();
+    	driver.get("http://www.google.com");
+    	
+    	// 1sn = 1000 ms
+    	Thread.sleep(1000);
+    	
+    	WebElement q = driver.findElement(By.xpath("//input[@name='q']"));
+    	//WebElement q = driver.findElement(By.cssSelector("input[name='q']"));
+    	q.sendKeys("codingbook");
+    	Thread.sleep(100);
+    	q.submit();  //<input type="button"
+    	//q.click(); //<button type="button"
+    	
+    	Thread.sleep(100);
+    	WebElement cbLink = driver.findElement(By.xpath("(//div[@class='g']//a)[1]"));
+    	cbLink.click();
+    	
+    	Thread.sleep(5000);
+    	
+    	//CodingBook.org sitesine ulaştık
+    	ArrayList<String> locators = new ArrayList<String>();
+    	locators.add("//li[@id='menu-item-6539']/a"); //Course
+    	locators.add("//li[@id='menu-item-6538']/a"); //Test Automation
+    	locators.add("//div[@id='learn-press-course-tabs']/ul/li[2]/a"); //Curriculum
+    	locators.add("//span[contains(text(), 'Basic Test Automation – Assignments')]"); //Basic Test Automation – Assignments
+    	locators.add("//a[contains(text(), 'youtube')]"); //Youtube
+    	
+    	for(String locator : locators) {
+    		WebElement elm = driver.findElement(By.xpath(locator));
+    		elm.click();
+    		Thread.sleep(2000);
+    	}
+    	
+//    	locators.forEach((locator)-> {
+//    		WebElement elm = driver.findElement(By.xpath(locator));
+//    		elm.click();
+//    		try {
+//				Thread.sleep(1000);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//    	});
     }
 }
