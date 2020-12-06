0e1c8b794b4b49bf170a67eee921726f01d6ee44
semih-git <cemcan34355@gmail.com>
2020-12-06 22:29:44 +0100

    i updated Lambda class.

--- FullTest/src/main/java/org/cb/ta/FullTest/Lambda.java
+++ FullTest/src/main/java/org/cb/ta/FullTest/Lambda.java
@@ -40,23 +40,19 @@ public class Lambda {
 		 * we do not need to write parameter type.
 		 * if there is only one parameter, no need to use parentheses.
 		 * if there is only one line in method body, no need to use curly brackets.
-		 * 
+		  
 		 * Functional Interface: Only one abstract method.
 		 * Runnable, Comparator, Consumer, Callable.
 		 */
 		
 		/**
-		 * METHOD
+		 * METHOD:
 		 * access modifier - return type - name - ( parameters ) { body }
-		 * public void printName(String name, int age) {
-				System.out.println("Hi, CB");
-		   }
-		 * 		 
-		 * ANONYMOUS METHOD
+		 * public void printName(String name, int age) {System.out.println("Hi, CB"); }
+		  		 
+		 * ANONYMOUS METHOD:
 		 * (parameters) -> { body }		 
-		 * (String name, int age)-> { 
-		 * 	System.out.println("Hi, CB"); 
-		 * }
+		 * (String name, int age) -> { System.out.println("Hi, CB");};
 		 */
 	}
 
